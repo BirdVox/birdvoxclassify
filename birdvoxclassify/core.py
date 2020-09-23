16933b5d0fc41c3ff38f7374a0784908d6758ef4
Jason Cramer <jason.t.cramer@gmail.com>
2020-09-23 14:49:03 -0400

    Add TaxoNet model into BVC

--- birdvoxclassify/core.py
+++ birdvoxclassify/core.py
@@ -11,8 +11,8 @@ import soundfile as sf
 
 from .birdvoxclassify_exceptions import BirdVoxClassifyError
 
-DEFAULT_MODEL_SUFFIX = "flat-multitask-convnet-v2_" \
-                       "tv1hierarchical-2e7e1bbd434a35b3961e315cfe3832fc"
+DEFAULT_MODEL_SUFFIX = "taxonet_tv1hierarchical" \
+                       "-7896de0a208a341730b315e90bf3e30f"
 DEFAULT_MODEL_NAME = "birdvoxclassify-{}".format(DEFAULT_MODEL_SUFFIX)
 
 
